08ca15e5cf1a87c52b2817913362504f69a7ed1c
Suman Kavi Bala <150148076+sumankavibala@users.noreply.github.com>
2024-07-30 18:04:14 +0530

    Sef 00 backend base setup (#2)
    * This commit included base setup files for this backend project
    
    * removed sql fetch query due to unwanted nature

--- main.ts
+++ main.ts
@@ -1,12 +1,30 @@
-import Express from "express";
-
+import Express, { response } from "express";
+import sql from "mssql";
 import dotenv from "dotenv";
 dotenv.config();
+
 
 const port = process.env.PORT;
 
 const app = Express();
 
-app.listen (port,()=>{
-    console.log("This is console log ---->App is successfully running---->on port number:"+port);  
+//MS SQL server configuration
+const config={
+    "user": "sa",
+    "password": "changeme",
+    "server":"1433",
+    "database":"test_db",
+    "options":{
+        "encrypt":false
+    }
+}
+
+sql.connect(config,()=>{
+    console.log("This is console log--->sql connect config--->MS-SQL DB connected successfully")
 })
+
+app.listen (port,()=>{
+    console.log("This is console log--->App is successfully running--->on port number:"+port);  
+})
+
+
